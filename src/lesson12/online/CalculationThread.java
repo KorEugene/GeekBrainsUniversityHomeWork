2a107a5254e99c4cd2e888e923eea104c19f5c6a
KorEugene <johnkorch@yandex.ru>
2021-03-05 17:43:00 +0300

    HW_2.5 - constant's name fix

--- src/lesson12/online/CalculationThread.java
+++ src/lesson12/online/CalculationThread.java
@@ -3,17 +3,17 @@ package lesson12.online;
 public class CalculationThread extends Thread {
 
     private final float[] ARRAY_FOR_CALCULATION;
-    private final int counter;
+    private final int COUNTER;
 
     CalculationThread(float[] arrayForCalculation, int counter, String name) {
         this.ARRAY_FOR_CALCULATION = arrayForCalculation;
-        this.counter = counter;
+        this.COUNTER = counter;
         this.setName(name);
     }
 
     @Override
     public void run() {
-        Main.recalculateArrayElements(ARRAY_FOR_CALCULATION, counter);
+        Main.recalculateArrayElements(ARRAY_FOR_CALCULATION, COUNTER);
         System.out.println(String.format("Thread %s finished the job!", getName()));
     }
 }
