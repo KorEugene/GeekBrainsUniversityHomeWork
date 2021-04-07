232f8d011655e0a2f39e8a13af67cf6fd0690fdc
Eugene Korchevoy <johnkorch@yandex.ru>
2021-04-07 14:11:38 +0300

    HW_3.5 - ready!

---
+++ src/lesson17/online/Car.java
@@ -0,0 +1,56 @@
+package lesson17.online;
+
+import java.util.concurrent.BrokenBarrierException;
+
+public class Car implements Runnable {
+
+    private static int CARS_COUNT;
+    private static boolean winner = false;
+
+    static {
+        CARS_COUNT = 0;
+    }
+
+    private Race race;
+    private int speed;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            MainClass.LINE.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            checkWinner(this);
+            MainClass.LINE.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static synchronized void checkWinner(Car c) {
+        if (!winner) {
+            System.out.println(c.name + " - WIN");
+            winner = true;
+        }
+    }
+}
---
+++ src/lesson17/online/MainClass.java
@@ -0,0 +1,30 @@
+package lesson17.online;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class MainClass {
+
+    public static final int CARS_COUNT = 4;
+    public static final CyclicBarrier LINE = new CyclicBarrier(CARS_COUNT + 1);
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+        try {
+            LINE.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            LINE.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
